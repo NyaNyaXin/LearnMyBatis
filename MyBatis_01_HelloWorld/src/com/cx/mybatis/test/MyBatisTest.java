256f04b7bd80c679435f1e79f419de7de1e2f210
NyaNyaXin <chenxin95013>
2017-05-18 19:03:51 +0800

    MyBatis学习之HelloWorld小结

--- MyBatis_01_HelloWorld/src/com/cx/mybatis/test/MyBatisTest.java
+++ MyBatis_01_HelloWorld/src/com/cx/mybatis/test/MyBatisTest.java
@@ -12,6 +12,20 @@ import org.junit.Test;
 import com.cx.mybatis.bean.Employee;
 import com.cx.mybatis.dao.EmployeeMapper;
 
+/**
+ * 1.接口式编程
+ * 		原生：		Dao		======>		DaoImpl
+ * 		MyBatis：	Mapper	======>		xxMapper.xml
+ * 
+ * 2.SqlSession对象代表和数据库的一次会话；用完必须关闭
+ * 3.SqlSession对象和connection一样，都是非线程安全的。每次使用都应该去获取新的对象
+ * 4.mapper接口没有实现类，但是mybatis会为这个接口生成一个代理对象。
+ * 		(将接口和xml进行绑定)
+ * 		EmployeeMapper empMapper = sqlSession.getmapper(EmployeeMapper.class);
+ * 5.两个重要的配置文件
+ * 		全局配置文件：包含数据库连接池信息，事务管理器信息。。。。等系统运行环境信息
+ * 		sql映射文件：保存了每一个sql语句的映射信息：将sql抽取出来
+ * **/
 public class MyBatisTest {
 
 	/**
