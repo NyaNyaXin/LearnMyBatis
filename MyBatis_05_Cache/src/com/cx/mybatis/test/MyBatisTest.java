9c236c79ccb35c863927021fa38788e6cfe4cb8e
NyaNyaXin <chenxin95013>
2017-05-27 12:04:40 +0800

    一级缓存失效的四种情况

--- MyBatis_05_Cache/src/com/cx/mybatis/test/MyBatisTest.java
+++ MyBatis_05_Cache/src/com/cx/mybatis/test/MyBatisTest.java
@@ -22,9 +22,15 @@ public class MyBatisTest {
 
 	/**
 	 * 两级缓存
-	 * 一级缓存：(本地缓存)
+	 * 一级缓存：(本地缓存)：sqlSession级别的缓存，一级缓存是一直开启的;SqlSession级别的一个Map
 	 * 	与数据库同一次会话期间查询到的数据会被放在本地缓存中。
 	 * 	以后如果需要获取相同的数据，直接从缓存中拿，不再去查数据库
+	 * 	
+	 * 	一级缓存失效的情况(没有使用到当前一级缓存的情况，效果就是，还需要再次向数据库发送sql语句)
+	 * 	1.SQLSession不同
+	 * 	2.sqlSession相同，查询条件不同(当前一级缓存中还没有这个数据)
+	 * 	3.SQLSession相同，两次查询之间执行了增删改操作(这次增删改可能对当前数据有影响)
+	 * 	4.SQLSession相同，手动清除了一级缓存中的数据
 	 * 二级缓存：(全局缓存)
 	 * @throws IOException 
 	 */
@@ -32,14 +38,27 @@ public class MyBatisTest {
 	public void testFristLevelCache() throws IOException {
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession session = sqlSessionFactory.openSession();
+		//1.SQLSession不同
+		//SqlSession session2 = sqlSessionFactory.openSession();
+		
 		try {
 			EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
 			Employee employee = mapper.getEmpById(1);
 			System.out.println(employee);
+			//EmployeeMapper mapper2 = session2.getMapper(EmployeeMapper.class);
+			//2.sqlSession相同，查询条件不同
+			
+			//3.SQLSession相同，两次查询之间执行了增删改操作
+//			mapper.addEmployee(new Employee(null, "testCache", "cache", "1"));
+//			System.out.println("数据添加");
+
+			//4.SQLSession相同，手动清除了一级缓存中的数据
+			session.clearCache();
 			Employee employee2 = mapper.getEmpById(1);
-			System.out.println(employee2==employee);
+			System.out.println(employee2);
 		} finally {
 			session.close();
+			//session2.close();
 		}
 	}
 }
