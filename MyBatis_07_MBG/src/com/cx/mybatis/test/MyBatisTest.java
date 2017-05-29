8f32ba39076062a62b19ae82a4c657f7d4f6b603
NyaNyaXin <chenxin95013>
2017-05-29 16:48:13 +0800

    Mybatis逆向工程(测试简单查询和带条件的复杂查询)

--- MyBatis_07_MBG/src/com/cx/mybatis/test/MyBatisTest.java
+++ MyBatis_07_MBG/src/com/cx/mybatis/test/MyBatisTest.java
@@ -3,20 +3,23 @@ package com.cx.mybatis.test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.InvalidConfigurationException;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import com.cx.mybatis.bean.Employee;
+import com.cx.mybatis.bean.EmployeeExample;
+import com.cx.mybatis.bean.EmployeeExample.Criteria;
+import com.cx.mybatis.dao.EmployeeMapper;
 
 public class MyBatisTest {
 
@@ -36,6 +39,50 @@ public class MyBatisTest {
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 		myBatisGenerator.generate(null);
+	}
+	
+//	@Test
+//	public void testMyBatis3Simple() throws IOException {
+//		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+//		SqlSession session = sqlSessionFactory.openSession();
+//		try {
+//			EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
+//			List<Employee> list = mapper.selectAll();
+//			for(Employee employee:list) {
+//				System.out.println(employee.getLastName());
+//			}
+//		} finally {
+//			session.close();
+//		}
+//	}
+	
+	@Test
+	public void testMyBatis3() throws IOException {
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
+			//xxxExample封装查询条件
+			//1.查询所有
+			//List<Employee> employees = mapper.selectByExample(null);
+			//2.查询员工名字中有e字母的，并且性别是1
+			//封装员工查询条件
+			EmployeeExample employeeExample = new EmployeeExample();
+			//创建一个Criteria，这个Criteria就是拼装查询条件
+			Criteria criteria = employeeExample.createCriteria();
+			criteria.andLastNameLike("%c%").andGenderEqualTo("1");
+			
+			Criteria criteria2 = employeeExample.createCriteria();
+			criteria2.andEmailLike("%c%");
+			employeeExample.or(criteria2);
+			List<Employee> list = mapper.selectByExample(employeeExample);
+
+			for(Employee employee:list) {
+				System.out.println(employee.getLastName()+":"+employee.getId());
+			}
+		} finally {
+			session.close();
+		}
 	}
 
 }
