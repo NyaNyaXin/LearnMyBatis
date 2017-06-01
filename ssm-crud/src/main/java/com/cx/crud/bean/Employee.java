7548330bbff391e0b7daad75a6327f4c4d7c70de
NyaNyaXin <chenxin95013>
2017-06-01 11:56:43 +0800

    SSM整合之搭建Spring单元测试环境测试Dao

--- ssm-crud/src/main/java/com/cx/crud/bean/Employee.java
+++ ssm-crud/src/main/java/com/cx/crud/bean/Employee.java
@@ -1,22 +1,33 @@
 package com.cx.crud.bean;
 
 public class Employee {
-    private Integer empId;
+	private Integer empId;
 
-    private String empName;
+	private String empName;
 
-    private String gender;
+	private String gender;
 
-    private String email;
+	private String email;
 
-    private Integer dId;
-    
-    //希望查詢員工同時查詢部門信息
-    private Department department;
-    
-    
+	private Integer dId;
 
-    public Department getDepartment() {
+	// 希望查詢員工同時查詢部門信息
+	private Department department;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(Integer empId, String empName, String gender, String email, Integer dId) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.gender = gender;
+		this.email = email;
+		this.dId = dId;
+	}
+
+	public Department getDepartment() {
 		return department;
 	}
 
@@ -25,42 +36,42 @@ public class Employee {
 	}
 
 	public Integer getEmpId() {
-        return empId;
-    }
+		return empId;
+	}
 
-    public void setEmpId(Integer empId) {
-        this.empId = empId;
-    }
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
 
-    public String getEmpName() {
-        return empName;
-    }
+	public String getEmpName() {
+		return empName;
+	}
 
-    public void setEmpName(String empName) {
-        this.empName = empName == null ? null : empName.trim();
-    }
+	public void setEmpName(String empName) {
+		this.empName = empName == null ? null : empName.trim();
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public void setGender(String gender) {
-        this.gender = gender == null ? null : gender.trim();
-    }
+	public void setGender(String gender) {
+		this.gender = gender == null ? null : gender.trim();
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email == null ? null : email.trim();
-    }
+	public void setEmail(String email) {
+		this.email = email == null ? null : email.trim();
+	}
 
-    public Integer getdId() {
-        return dId;
-    }
+	public Integer getdId() {
+		return dId;
+	}
 
-    public void setdId(Integer dId) {
-        this.dId = dId;
-    }
+	public void setdId(Integer dId) {
+		this.dId = dId;
+	}
 }
---
+++ ssm-crud/src/main/java/com/cx/crud/test/TestMapper.java
@@ -0,0 +1,63 @@
+package com.cx.crud.test;
+
+import java.util.UUID;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+/**
+ * 测试Dao层的工作
+ * 
+ * 推荐Spring项目就是用Spring的单元测试，可以自动注入需要的组件
+ * 1.导入SpringTest模块
+ * 2.使用@ContextConfiguration注解指定Spring配置文件的注解
+ * 3.直接autowired要使用的组件即可
+ * **/
+
+import com.cx.crud.bean.Department;
+import com.cx.crud.bean.Employee;
+import com.cx.crud.dao.DepartmentMapper;
+import com.cx.crud.dao.EmployeeMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class TestMapper {
+	@Autowired
+	DepartmentMapper departmentMapper;
+	@Autowired
+	EmployeeMapper employeeMapper;
+	@Autowired
+	SqlSession sqlSession;
+
+	/**
+	 * 测试Departmentmapper
+	 **/
+	@Test
+	public void testCRUD() {
+		/*
+		 * //1.创建SpringIOC容器 ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("applicationContext.xml"); //2.从容器中获取mapper
+		 * DepartmentMapper departmentMapper = context.getBean(DepartmentMapper.class);
+		 */
+		System.out.println(departmentMapper);
+		// 1.插入几个部门
+		// departmentMapper.insertSelective(new Department(null,"开发部"));
+		// departmentMapper.insertSelective(new Department(null,"测试部"));
+		// 2.生成员工数据，测试员工插入
+		// employeeMapper.insertSelective(new Employee(null, "chenxin", "M",
+		// "chenxin@163.com", 1));
+		// employeeMapper.insertSelective(new Employee(null, "丁健辉", "M",
+		// "chenxin@163.com", 2));
+		// //3.批量插入多个员工：使用可以执行批量操作的sqlsession
+
+		EmployeeMapper employeeMapper2 = sqlSession.getMapper(EmployeeMapper.class);
+		for (int i = 0; i < 1000; i++) {
+			String uid = UUID.randomUUID().toString().substring(0, 5) + "" + i;
+			employeeMapper2.insertSelective(new Employee(null, uid, "M", uid + "@163.com", 2));
+		}
+		System.out.println("批量");
+	}
+}
