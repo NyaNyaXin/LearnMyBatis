4e222aa2e681aa2b78349ff3578599e79ea9d279
NyaNyaXin <chenxin95013>
2017-06-01 12:40:09 +0800

    SSM整合之分页代码实现

---
+++ ssm-crud/src/main/java/com/cx/crud/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.cx.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cx.crud.bean.Employee;
+import com.cx.crud.dao.EmployeeMapper;
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeMapper employeeMapper;
+	/**
+	 * 查询所有员工
+	 * **/
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return employeeMapper.selectByExampleWithDept(null);
+	}
+	
+}
